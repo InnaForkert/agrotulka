9bd8fd7edf89e2bb1bb99915cc5e07be4664e78e
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2023-01-13 21:53:29 +0200

    optimize images

--- src/utils/chapters.js
+++ src/utils/chapters.js
@@ -1,57 +1,57 @@
 import chapter0img1 from "../img/intro/1.jpg";
 
-import chapter1img1 from "../img/chapter1/1.png";
-import chapter1img2 from "../img/chapter1/2.png";
-import chapter1img3 from "../img/chapter1/3.png";
+import chapter1img1 from "../img/chapter1/1.jpg";
+import chapter1img2 from "../img/chapter1/2.jpg";
+import chapter1img3 from "../img/chapter1/3.jpg";
 import chapter1img4 from "../img/chapter1/4.jpg";
-import chapter1img5 from "../img/chapter1/5.png";
+import chapter1img5 from "../img/chapter1/5.jpg";
 
-import chapter1_1img1 from "../img/chapter1.1/1.png";
+import chapter1_1img1 from "../img/chapter1.1/1.jpg";
 import chapter1_1img2 from "../img/chapter1.1/2.jpg";
 import chapter1_1img3 from "../img/chapter1.1/3.jpg";
 
-import chapter1_2img1 from "../img/chapter1.2/1.png";
-import chapter1_2img2 from "../img/chapter1.2/2.png";
-import chapter1_2img3 from "../img/chapter1.2/3.png";
-import chapter1_2img4 from "../img/chapter1.2/4.png";
-import chapter1_2img5 from "../img/chapter1.2/5.png";
-import chapter1_2img6 from "../img/chapter1.2/6.png";
-import chapter1_2img7 from "../img/chapter1.2/7.png";
-import chapter1_2img8 from "../img/chapter1.2/8.png";
-import chapter1_2img9 from "../img/chapter1.2/9.png";
-import chapter1_2img10 from "../img/chapter1.2/10.png";
+import chapter1_2img1 from "../img/chapter1.2/1.jpg";
+import chapter1_2img2 from "../img/chapter1.2/2.jpg";
+import chapter1_2img3 from "../img/chapter1.2/3.jpg";
+import chapter1_2img4 from "../img/chapter1.2/4.jpg";
+import chapter1_2img5 from "../img/chapter1.2/5.jpg";
+import chapter1_2img6 from "../img/chapter1.2/6.jpg";
+import chapter1_2img7 from "../img/chapter1.2/7.jpg";
+import chapter1_2img8 from "../img/chapter1.2/8.jpg";
+import chapter1_2img9 from "../img/chapter1.2/9.jpg";
+import chapter1_2img10 from "../img/chapter1.2/10.jpg";
 
-import chapter1_3img1 from "../img/chapter1.3/1.png";
-import chapter1_3img2 from "../img/chapter1.3/2.png";
-import chapter1_3img3 from "../img/chapter1.3/3.png";
-import chapter1_3img4 from "../img/chapter1.3/4.png";
-import chapter1_3img5 from "../img/chapter1.3/5.png";
-import chapter1_3img6 from "../img/chapter1.3/6.png";
-import chapter1_3img7 from "../img/chapter1.3/7.png";
+import chapter1_3img1 from "../img/chapter1.3/1.jpg";
+import chapter1_3img2 from "../img/chapter1.3/2.jpg";
+import chapter1_3img3 from "../img/chapter1.3/3.jpg";
+import chapter1_3img4 from "../img/chapter1.3/4.jpg";
+import chapter1_3img5 from "../img/chapter1.3/5.jpg";
+import chapter1_3img6 from "../img/chapter1.3/6.jpg";
+import chapter1_3img7 from "../img/chapter1.3/7.jpg";
 import chapter1_3img8 from "../img/chapter1.3/8.jpg";
 
-import chapter1_4img1 from "../img/chapter1.4/1.png";
-import chapter1_4img2 from "../img/chapter1.4/2.png";
-import chapter1_4img3 from "../img/chapter1.4/3.png";
-import chapter1_4img4 from "../img/chapter1.4/4.png";
-import chapter1_4img5 from "../img/chapter1.4/5.png";
-import chapter1_4img6 from "../img/chapter1.4/6.png";
+import chapter1_4img1 from "../img/chapter1.4/1.jpg";
+import chapter1_4img2 from "../img/chapter1.4/2.jpg";
+import chapter1_4img3 from "../img/chapter1.4/3.jpg";
+import chapter1_4img4 from "../img/chapter1.4/4.jpg";
+import chapter1_4img5 from "../img/chapter1.4/5.jpg";
+import chapter1_4img6 from "../img/chapter1.4/6.jpg";
 
-import chapter1_5img1 from "../img/chapter1.5/1.png";
-import chapter1_5img2 from "../img/chapter1.5/2.png";
+import chapter1_5img1 from "../img/chapter1.5/1.jpg";
+import chapter1_5img2 from "../img/chapter1.5/2.jpg";
 import chapter1_5img3 from "../img/chapter1.5/3.jpg";
-import chapter1_5img4 from "../img/chapter1.5/4.png";
-import chapter1_5img5 from "../img/chapter1.5/5.png";
+import chapter1_5img4 from "../img/chapter1.5/4.jpg";
+import chapter1_5img5 from "../img/chapter1.5/5.jpg";
 
-import chapter1_6img1 from "../img/chapter1.6/1.png";
+import chapter1_6img1 from "../img/chapter1.6/1.jpg";
 import chapter1_6img2 from "../img/chapter1.6/2.jpg";
-import chapter1_6img3 from "../img/chapter1.6/3.png";
-import chapter1_6img4 from "../img/chapter1.6/4.png";
-import chapter1_6img5 from "../img/chapter1.6/5.png";
-import chapter1_6img6 from "../img/chapter1.6/6.png";
-import chapter1_6img7 from "../img/chapter1.6/7.png";
-import chapter1_6img8 from "../img/chapter1.6/8.png";
-import chapter1_6img9 from "../img/chapter1.6/9.png";
+import chapter1_6img3 from "../img/chapter1.6/3.jpg";
+import chapter1_6img4 from "../img/chapter1.6/4.jpg";
+import chapter1_6img5 from "../img/chapter1.6/5.jpg";
+import chapter1_6img6 from "../img/chapter1.6/6.jpg";
+import chapter1_6img7 from "../img/chapter1.6/7.jpg";
+import chapter1_6img8 from "../img/chapter1.6/8.jpg";
+import chapter1_6img9 from "../img/chapter1.6/9.jpg";
 
 export const chapters = {
   Вступ: [chapter0img1],
